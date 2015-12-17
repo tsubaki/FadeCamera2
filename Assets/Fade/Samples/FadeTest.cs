684290abb1b6d5314b28a3a71e08529b26e01d14
tsubaki <ruitomo.ex@gmail.com>
2015-12-18 01:51:59 +0900

    update unitypackage

--- Assets/Fade/Samples/FadeTest.cs
+++ Assets/Fade/Samples/FadeTest.cs
@@ -25,6 +25,10 @@ using System.Collections;
 
 public class FadeTest : MonoBehaviour 
 {
+	private bool isMainColor = false;
+	[SerializeField] Color color1 = Color.white, color2 = Color.white;
+	[SerializeField] UnityEngine.UI.Image image = null;
+
 	[SerializeField]
 	CanvasGroup group = null;
 
@@ -36,6 +40,8 @@ public class FadeTest : MonoBehaviour
 		group.blocksRaycasts = false;
 		fade.FadeIn (1, () =>
 		{
+			image.color = (isMainColor) ? color1 : color2;
+			isMainColor = !isMainColor;
 			fade.FadeOut(1, ()=>{
 				group.blocksRaycasts = true;
 			});
